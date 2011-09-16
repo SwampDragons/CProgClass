7d583f3524b2930372ab181e84d44243d7828105
Megan Marsh <megan.n.marsh@gmail.com>
2011-09-16 15:53:07 -0700

    started calculator code"

---
+++ Calc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    double result = 0.0, input;
+    printf ("calculator is on.\n");
+    printf ("result = %d \n", &result);
+    scanf ("%d", &input);
+
