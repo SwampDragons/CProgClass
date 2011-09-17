f163b80c50b653d186b6c052f07316c7f47602ef
Megan Marsh <megan.n.marsh@gmail.com>
2011-09-17 11:14:29 -0700

    made Hello World more badass by adding multi-language functionality

--- test.c
+++ test.c
@@ -6,7 +6,22 @@ void print_title ( void );
 
 int main ( void ) 
 {
-    print_title();
+    int lang;
+    printf("French(1) or English(2)?");
+    scanf("%d", &lang);
+    if (lang == 1)
+    {
+        int n=0;
+        while (n<10)
+        {
+            printf("Bonjour, Monde!\n");
+            n++;
+        }
+    }
+    else if (lang == 2)
+        print_title();
+    else 
+        return 1; //error code
     return 0;
 }
 
