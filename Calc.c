311f68b14af8543d592d726f36513832f35b2e6f
Megan Marsh <megan.n.marsh@gmail.com>
2011-09-17 10:40:35 -0700

    modified calc and added test.

--- Calc.c
+++ Calc.c
@@ -2,9 +2,24 @@
 #include <stdlib.h>
 
 int main ()
+
 {
-    double result = 0.0, input;
-    printf ("calculator is on.\n");
-    printf ("result = %d \n", &result);
-    scanf ("%d", &input);
+    int num1;
+    double result;
+   
+    result = 0.0;
+
+    printf ("calculator is on.\n Enter an operator and a number separated by a space.\n");
+    printf ("result = %f \n", &result);
+   
+    /*
+    scanf ("%f %f", &operator, &num1);
+    if (operator = + )
+        print ("%f + %f is %f", result, num1, result = result + num1) 
+    return ( operator constant);
+    */
+    
+    return 0;
+
+}
 
---
+++ test.c
@@ -0,0 +1,22 @@
+/*I like being code monkey!*/
+
+#include <stdio.h>
+
+void print_title ( void );
+
+int main ( void ) 
+{
+    print_title();
+    return 0;
+}
+
+/*************************print_title()*************************************/
+
+void print_title( void )
+
+{
+    int n;
+    for (n=0; n<10; n++)
+        printf( "Hello World!\n" );
+}
+
