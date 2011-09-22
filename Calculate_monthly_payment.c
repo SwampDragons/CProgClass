7b110bdb2af7ce6597a11b789376410ce2162667
Megan Marsh <megan.n.marsh@gmail.com>
2011-09-21 20:00:27 -0700

    updated code for calculate monthly payment -- changed int to double

--- Calculate_monthly_payment.c
+++ Calculate_monthly_payment.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+
 /*this program is designed to perform a simple interest calculation, using the formula of:
  *
  * monthly pay = {rate + rate/[(1+rate)^months-1]}x(principle)
  * 
  * this formula should compute the required monthly payment required to pay down the principle, and also the total payment
  */
+
 int main (void)
+
 {
-    int rate; int term; int principle; double  monthpay; double totalpay; double intexp;
+    double rate; double term; double principle; double  monthpay; double totalpay; double intexp;
 
 /**********************collect input from user**********************/
 
-    printf("This program calculates monthly payments. \nPlease enter the rate.");
-    scanf("%d", &rate);
-    printf("Now enter the term of the payment, in months. \n" );
-    scanf("%d", &term);
-    printf("Finally, enter the principle, or how much you owe.");
-    scanf("%d", &principle);
+    printf("This program calculates monthly payments. \nPlease enter the rate.\n");
+    scanf("%lf", &rate);
+    printf("Now enter the term of the payment, in years. \n" );
+    scanf("%lf", &term);
+    printf("Finally, enter the principle, or how much you owe.\n");
+    scanf("%lf", &principle);
 
 /************************ calculations: ****************************/
 
-    monthpay = (rate + rate/((1 + rate)^term -1))*principle;
+    rate = rate/1200;
+    term = term*12;
+    monthpay = (rate + rate/((1 + rate)^term*-1))*principle;
     totalpay = monthpay*term;
     intexp = totalpay - principle;
 
