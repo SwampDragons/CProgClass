ae093909971dd7c8059778a1d910dad84a98a718
Megan Marsh <megan.n.marsh@gmail.com>
2011-12-03 23:38:54 -0800

    dding 8 and 1 files

---
+++ homework/1.c
@@ -0,0 +1,16 @@
+/*
+ * the program cp.c makes a copy of a file. It uses getc () and putc () to do the input and output. 
+ * Write four new versions of a file copy program. 
+ * Each of the programs should have the same functionality as cp.c 
+ *
+ * a. In the first version, use fgetc () to do the input and fputc () to do the output. 
+ * b. In the second version, use fgets () to do the input and fputs () to do the output. 
+ *
+ * c. In the third version, use fread () to do the input and fwrite () to do the output. 
+ *
+ * d. In the fourth version, use fscanf () to do the input and fprintf () to do the output. 
+ *
+ * e. Compare the sizes of the object code from all five versions of the file copy programs. 
+ * If the facilities for testing execution efficiency are available on your system, 
+ * test the efficiency of each version. Report on your findings.  
+ * /
---
+++ homework/8.c
@@ -0,0 +1,11 @@
+/* a.  Write a function in C that takes an array of char and a char ** expression as parameters. 
+ * The array of type char would contain a string the possibly starts with a sequence of decimal digits. 
+ * Have the function convert the sequence of decimal digits it finds to type int and return 
+ * the converted value. Have the function set the contents of its second parameter (a char * value) 
+ * to point to the first character in the array that was not converted. 
+ * For example, if the function was called convert () and the call to convert () was 
+ * intval = convert (array, & charptr);
+ * where array contained the string 274ADAMS/0
+ * then after the call, intval would contain the 274, and charptr would point to the ‘A’ in the string.
+ */
+
