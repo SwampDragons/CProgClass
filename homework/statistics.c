ca23c4227bab03102936060be2623396cc5e92c8
Megan Marsh <megan.n.marsh@gmail.com>
2011-12-03 23:08:10 -0800

    created assignment statistics.c  Please enter the commit message for your changes. Lines starting

---
+++ homework/statistics.c
@@ -0,0 +1,34 @@
+/* 
+ * menu-driven mini-statistics package. A user should be able to enter up to 200 items of float data.  
+ * The program should calculate the number of items in the data, the mean, the standard deviation, 
+ * the variance, the median, and the mode of the data. A sample run follows. 
+ * The symbol <EOF> in the sample run below should be replaced with CTRL-Z or CTRL-D 
+ * or the end-of-file symbol on your system.
+ *
+ * Mini-Stat Package 
+ * -----------------------
+ *  This program will perform the following:
+ *  1) Enter data. 
+ *  2) Display the data and the following statistics: 
+ *  the number of date item, the high and low values in the data, the mean, median, mode, 
+ *       variance and standard deviation.
+ *  3) Quit the Program
+ *  --------------------------------------------------------
+ *
+ * Your Choice? 1
+ * Enter one data item after each prompt. Press return after each one. 
+ * Signal with <EOF> when you are done with data input. 
+ * Item #1        :    25
+ * Item #2   :    36 
+ * Item #3       :    27.5   
+ * Item #4       :    28
+ * Item #5       :    32
+ * Item #6       :    33.25
+ * Item #7       :    <EOF>
+ * This program will perform the following:
+ * 1) Enter data. 
+ * 2) Display the data and the following statistics: 
+ * the number of date item, the high and low values in the data, the mean, median, mode, 
+ * variance and standard deviation.
+ * 3) Quit the Program
+ */
