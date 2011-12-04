e1b018df6ac31df2d6a366752349b3b0354efc1b
Megan Marsh <megan.n.marsh@gmail.com>
2011-12-03 23:39:11 -0800

    adding 12.c

---
+++ homework/12.c
@@ -0,0 +1,16 @@
+/* a. Write a C function that will input values into an array of type int. 
+ * Have the array and the number of elements in the array passed as parameters to your function. 
+ * Use a local pointer variable to traverse the array. 
+ *
+ * b. Write a C function that will output the contents of an array of type int. 
+ * Have the array and the number of elements in the array passed as parameters. 
+ * Use a local pointer variable to traverse the array
+ * */
+#include <stdio.h>
+#include<stdlib.h>
+
+int main (void)
+{
+    int i;
+    printf("enter values you want in array");
+
